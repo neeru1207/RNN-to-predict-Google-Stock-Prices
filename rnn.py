a954ee9dc9a6739f789dada1a44524c17ece3a7f
Neeramitra Reddy <46085683+neeru1207@users.noreply.github.com>
2019-12-27 15:28:46 +0530

    Update rnn.py

--- rnn.py
+++ rnn.py
@@ -1,7 +1,5 @@
 # Recurrent Neural Network
 
-
-
 # Part 1 - Data Preprocessing
 
 # Importing the libraries
@@ -29,44 +27,38 @@ X_train, y_train = np.array(X_train), np.array(y_train)
 # Reshaping
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 
-
-
 # Part 2 - Building the RNN
 
-# Importing the Keras libraries and packages
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
+# Importing Tensorflow
+import tensorflow as tf
 
 # Initialising the RNN
-regressor = Sequential()
+regressor = tf.keras.models.Sequential()
 
 # Adding the first LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
-regressor.add(Dropout(0.2))
+regressor.add(tf.keras.layers.LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(tf.keras.layers.Dropout(0.2))
 
 # Adding a second LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
+regressor.add(tf.keras.layers.LSTM(units = 50, return_sequences = True))
+regressor.add(tf.keras.layers.Dropout(0.2))
 
 # Adding a third LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
+regressor.add(tf.keras.layers.LSTM(units = 50, return_sequences = True))
+regressor.add(tf.keras.layers.Dropout(0.2))
 
 # Adding a fourth LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50))
-regressor.add(Dropout(0.2))
+regressor.add(tf.keras.layers.LSTM(units = 50))
+regressor.add(tf.keras.layers.Dropout(0.2))
 
 # Adding the output layer
-regressor.add(Dense(units = 1))
+regressor.add(tf.keras.layers.Dense(units = 1))
 
 # Compiling the RNN
 regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 
 # Fitting the RNN to the Training set
 regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
-
 
 
 # Part 3 - Making the predictions and visualising the results
